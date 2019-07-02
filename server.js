35755ce4bb15c94e6dc932210fbcf4f8f0f325ec
Vilbak <vilbak86@gmail.com>
2019-07-02 08:41:53 +0300

    developer dependencies of server and  client

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
+/* eslint-disable no-path-concat */
 /* eslint-disable standard/object-curly-even-spacing */
 /* eslint-disable object-curly-spacing */
 const express = require('express')
-
+const connectDB = require('./config/db')
 const bodyParser = require('body-parser')
-
 const app = express()
+// const logger = require('./server/middleware/logger') 
 
-const logger = require('./server/middleware/logger')
 const modules = require('./server/modules/')
 
+connectDB()
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-logger.info('What rolls down stairs')
-logger.info('alone or in pairs,')
-logger.info('and over your neighbors dog?')
-logger.warn('Whats great for a snack,')
-logger.info('And fits on your back?')
-logger.error('')
-
 app.use(bodyParser.json())
 
+// const loggerstream = {
+//   write: function (message, encoding) {
+//     logger.info(message)
+//   }
+// }
+// app.use(require('morgan')('combined', { 'stream': loggerstream }))
 app.use('/', modules)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`
