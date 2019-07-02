3b266ccca8bb775850d6cfc876cdd0af015a48e6
Vilbak <vilbak86@gmail.com>
2019-07-02 09:04:44 +0300

    implemented components for frontend

---
+++ client/src/components/layout/layout.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+ import {BrowseRouter, Link} from 'react-router-dom'
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">MovieDB</h1>
+                <p className="lead">
+                  {' '}
+                  Reality is a thing of the past
+                </p>
+                <hr />
+                <Link  to ='/register' className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to = '/login' className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
